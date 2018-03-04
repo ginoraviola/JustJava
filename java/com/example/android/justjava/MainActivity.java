b1d87111f9209167165014d32f2804069b69bd84
gino <ginoraviola@gmail.com>
2018-03-04 18:04:09 -0300

    features added

--- java/com/example/android/justjava/MainActivity.java
+++ java/com/example/android/justjava/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.android.justjava;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.CheckResult;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,8 +20,12 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int numberOfCoffees = 0;
+    int numberOfCoffees = 2;
     int pricePerCup = 5;
+    int whippedCreamPrice = 1;
+    int chocolatePrice = 2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,39 +36,84 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        String summaryMessage = createOrderSummary(price);
-        displayMessage(summaryMessage);
+        int price = 0;
+        // Gets the user's name
+        EditText userName = (EditText) findViewById(R.id.name_editText);
+        String name = userName.getText().toString();
+
+        // Figure out if the user wants whipped cream topping
+        CheckBox whippedCreamCB = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCB.isChecked();
+
+        // Figure out if the user wants chocolate topping
+        CheckBox chocolateCB = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCB.isChecked();
+
+        price = calculatePrice(hasWhippedCream, hasChocolate);
+        String summaryMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
+        sendEmail(summaryMessage, name);
+
+
+
 
     }
+
     /**
      * Calculates the price of the order.
      *
+     * @param hasChocolate    is whether or not the user wants chocolate topping
+     * @param hasWhippedCream is whether or not the user wants whipped cream topping
      * @return total price
      */
-    public int calculatePrice(){
-        int price = numberOfCoffees * pricePerCup;
+    public int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
+
+        int price = pricePerCup;
+        if (hasWhippedCream) {
+            price += whippedCreamPrice;
+        }
+        if (hasChocolate) {
+            price += chocolatePrice;
+        }
+
+        price = numberOfCoffees * price;
         return price;
     }
 
-    public String createOrderSummary(int price){
-        String name = "Gino Raviola";
-        String summary = "Name: " + name + "\nQuantity: " + numberOfCoffees +
-                "\nTotal: " + price + "\nThank You!";
+    /**
+     * @param price           of the order
+     * @param addWhippedCream is whether or not the user wants whipped cream topping
+     * @param addChocolate    is whether or not the user wants chocolate topping
+     * @param name            of the user
+     * @return text summary
+     */
+    public String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, String name) {
+
+        String summary = "Name: " + name;
+        summary += "\nAdd Whipped Cream? " + addWhippedCream;
+        summary += "\nAdd Chocolate? " + addChocolate;
+        summary += "\nQuantity: " + numberOfCoffees;
+        summary += "\nTotal: $" + price;
+        summary += "\nThank You!";
         return summary;
     }
 
-    public void increment(View view){
-        numberOfCoffees++;
+    public void increment(View view) {
+
+        if (numberOfCoffees < 100) {
+            numberOfCoffees++;
+        }
         displayQuantity(numberOfCoffees);
 
     }
-    public void decrement(View view){
-        if(numberOfCoffees>=1){
+
+    public void decrement(View view) {
+
+        if (numberOfCoffees > 1) {
             numberOfCoffees--;
         }
         displayQuantity(numberOfCoffees);
     }
+
     /**
      * This method displays the given quantity value on the screen.
      */
@@ -68,11 +124,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method displays the given text on the screen.
+     * This method triggers the email app to send the order summary
+     * @param summary of the order
+     * @param name to know whose order it is.
      */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
+    private void sendEmail(String summary, String name){
+
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "JustJava order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, summary );
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
+
 
 }
